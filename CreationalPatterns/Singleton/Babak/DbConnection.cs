1fe5965b2e43610bbb6caa9b4513d0aee4c52ba6
babak.abbasi.develop <babak.abbasi.develop@gmail.com>
2022-01-21 10:08:17 +0330

    Added: Singelton

---
+++ CreationalPatterns/Singleton/Babak/DbConnection.cs
@@ -0,0 +1,18 @@
+﻿namespace DesignPatterns.CreationalPatterns.Singleton.Babak
+{
+    class DbConnection
+    {
+        private DbConnection dbConnection;
+        private DbConnection() 
+        {
+        }
+
+        public DbConnection GetDbConnection() 
+        {
+            if (dbConnection == null)
+                dbConnection = new();
+
+            return dbConnection;
+        }
+    }
+}
