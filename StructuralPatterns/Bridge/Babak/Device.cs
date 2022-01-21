20a4df41a99d85ac0ec62a53740096b0d45f4f18
babak.abbasi.develop <babak.abbasi.develop@gmail.com>
2022-01-21 09:25:09 +0330

    Added: Bridge pattern

---
+++ StructuralPatterns/Bridge/Babak/Device.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.StructuralPatterns.Bridge.Babak
+{
+    interface Device
+    {
+        void Enable();
+        void Disable();
+        bool IsEnabled();
+        void SetVolume(int volume);
+        int GetVolume();
+        void SetChannel(int channel);
+        int GetChannel();
+    }
+}
---
+++ StructuralPatterns/Bridge/Babak/Remote.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.StructuralPatterns.Bridge.Babak
+{
+    class Remote
+    {
+        private Device Device;
+
+        internal void TogglePower() 
+        {
+            if (Device.IsEnabled())
+                Device.Disable();
+            else Device.Enable();
+        }
+
+        internal void VolumeUp(int count) 
+        {
+            var deviceVolume = Device.GetVolume();
+            var newVolume = deviceVolume + count;
+
+            Device.SetVolume(newVolume);
+        }
+        internal void VolumeDown(int count)
+        {
+            var deviceVolume = Device.GetVolume();
+            var newVolume = deviceVolume - count;
+
+            Device.SetVolume(newVolume);
+        }
+
+        internal void ChannelUp(int count)
+        {
+            var deviceChannel = Device.GetChannel();
+            var newChannel = deviceChannel + count;
+
+            Device.SetChannel(newChannel);
+        }
+        internal void ChannelDown(int count)
+        {
+            var deviceChannel = Device.GetChannel();
+            var newChannel = deviceChannel - count;
+
+            Device.SetChannel(newChannel);
+        }
+    }
+}
