e0a87e6023e900b73a665dcab04de46d0641c959
babak.abbasi.develop <babak.abbasi.develop@gmail.com>
2022-03-22 15:08:30 +0430

    Update: Composite pattern

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@
 using DesignPatterns.Facade;
 using DesignPatterns.CreationalPatterns;
 using System;
+using DesignPatterns.StructuralPatterns.Composite;
 
 namespace DesignPatterns
 {
@@ -12,43 +13,58 @@ namespace DesignPatterns
     {
         static void Main(string[] args)
         {
-            string userName = "";
-            string password = "";
+            //string userName = "";
+            //string password = "";
 
-            // without FACADE design pattern
-            // ---------------------------------------------------------------------
-            // var validationService = new ValidationService();
-            // if(validationService.ISValidated(userName,password))
-            // {
-            //     var authenticationService = new AuthenticationService();
-            //     if(authenticationService.IsAuthenticated(userName,password))
-            //     {
-            //         var authorizationService  = new AuthorizationService();
-            //         if(authorizationService.IsAuthorized(userName,password))
-            //             System.Console.WriteLine("User loged in !");
-            //     }
-            // }
-            //------------------------------------------------------------------------
+            //// without FACADE design pattern
+            //// ---------------------------------------------------------------------
+            //// var validationService = new ValidationService();
+            //// if(validationService.ISValidated(userName,password))
+            //// {
+            ////     var authenticationService = new AuthenticationService();
+            ////     if(authenticationService.IsAuthenticated(userName,password))
+            ////     {
+            ////         var authorizationService  = new AuthorizationService();
+            ////         if(authorizationService.IsAuthorized(userName,password))
+            ////             System.Console.WriteLine("User loged in !");
+            ////     }
+            //// }
+            ////------------------------------------------------------------------------
 
-            //FACADE solution 
-            var loginFacade = new LoginFacade();
-            if(loginFacade.CanLogin(userName,password))
-                WriteLine("user loged in !");
+            ////FACADE solution 
+            //var loginFacade = new LoginFacade();
+            //if(loginFacade.CanLogin(userName,password))
+            //    WriteLine("user loged in !");
 
-            var firstCustomer = new Customer()
-            {
-                Name = "Alireza",
-                LastName = "Mori",
-                PhoneNumber = "+9890122222222"
-            };
-            var firstCustomerClone = firstCustomer.Clone();
+            //var firstCustomer = new Customer()
+            //{
+            //    Name = "Alireza",
+            //    LastName = "Mori",
+            //    PhoneNumber = "+9890122222222"
+            //};
+            //var firstCustomerClone = firstCustomer.Clone();
 
-            var modernHouse = new HouseDirector().CreateModernHouse();
-            WriteLine(modernHouse.WallType);
+            //var modernHouse = new HouseDirector().CreateModernHouse();
+            //WriteLine(modernHouse.WallType);
 
-            var xml = new StructuralPatterns.Adapter(new StructuralPatterns.XML());
-            var x = xml.Convert();
-            
+            //var xml = new StructuralPatterns.Adapter(new StructuralPatterns.XML());
+            //var x = xml.Convert();
+
+            var box = new Box();
+            var product = new Product() { Price = 10 };
+            var productTwo = new Product() { Price = 20 };
+
+            box.Add(product);
+            box.Add(productTwo);
+
+            var boxTwo = new Box();
+            var productThree = new Product() { Price = 30 };
+            boxTwo.Add(box);
+            boxTwo.Add(productThree);
+
+            var priceOfProducts = boxTwo.GetPrice();
+            Console.WriteLine(priceOfProducts);
+
             ReadLine();
         }
     }
---
+++ StructuralPatterns/Composite/Box.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace DesignPatterns.StructuralPatterns.Composite
+{
+    public class Box : IProduct
+    {
+        private List<IProduct> children = new List<IProduct>();
+
+        public void Add(IProduct product) 
+        {
+            children.Add(product);
+        }
+
+        public void Remove(IProduct product) 
+        {
+            children.Remove(product);
+        }
+
+        public decimal GetPrice() 
+        {
+            decimal returnValue = 0;
+
+            foreach (var child in children)
+                returnValue += child.GetPrice();
+            
+            return returnValue;
+        }
+    }
+}
